b2f9aeaca09d156cd86b25713881482987df245f
Om-Prakash-Chaurasia <omchaurasia2024@gmail.com>
2025-11-26 15:06:30 +0530

    mongodb connection established.

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-const express = require("express");
+const express = require("express"); // importing express ~ importing http and events modules
+const mongoose = require("mongoose"); // importing mongoose
 
-const app = express();
-// app.use(express.json());
+const app = express(); // http.createServer()
+// app.use(express.json()); // Middleware
 
+// GET api
 app.get("/", (req, res) => {
   res.send("This is my first express api");
 });
 
+// POST api
 app.post("/", (req, res) => {
   res.json({ msg: "You can enter details here" });
 });
 
-app.listen(1010, () => {
-  console.log("Server is up and running on port 3000");
+// URI :-
+const connectDB = async () => {
+  await mongoose.connect("mongodb://localhost:27017/augBatch");
+};
+
+connectDB()
+  .then(() => {
+    console.log("✅ Connection with Database established.");
+  })
+  .catch((err) => {
+    console.log("❌ Unable to connect, please try again later.", err);
+  });
+
+// server.listen();
+const PORT = 1010;
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
 });
